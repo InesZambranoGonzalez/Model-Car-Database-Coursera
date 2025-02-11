efa4257a59978d2b6c7249f6f837e3d85655ef1e
InesZambranoGonzalez <zambranogonzalezines@gmail.com>
2025-02-11 16:00:33 +0100

    Update in forms

--- proyecto Mintclassics.sql
+++ proyecto Mintclassics.sql
@@ -1,5 +1,6 @@
 
 /* */
+CREATE OR REPLACE VIEW `profit_per_warehouses` AS
 SELECT 
     warehouses.warehouseCode,
     warehouses.warehousePctCap,
@@ -11,6 +12,7 @@ INNER JOIN products ON warehouses.warehouseCode = products.warehouseCode
 GROUP BY warehouses.warehouseCode;
 
 /* */
+CREATE OR REPLACE VIEW `popular_products_bought` AS
 SELECT
 	products.MSRP,
 	orderdetails.productCode,
@@ -19,10 +21,16 @@ FROM orderdetails
 INNER JOIN products ON orderdetails.productCode = products.productCode
 GROUP BY orderdetails.productCode 
 ORDER BY total_times_bought DESC; 
-	
-	
 
-
+CREATE OR REPLACE VIEW `less_than_average_performance_by_products` AS
+SELECT popular_products_bought.total_times_bought , products.*
+FROM popular_products_bought 
+right JOIN products ON popular_products_bought.productCode = products.productCode
+WHERE total_times_bought < (SELECT (AVG(total_times_bought)-1) FROM popular_products_bought);
+	
+SELECT * from less_than_average_performance_by_products;
+SELECT * from popular_products_bought;
+SELECT * from profit_per_warehouses;
 
 
 
