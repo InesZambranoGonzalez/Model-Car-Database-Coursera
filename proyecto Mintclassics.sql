1881510436c088cbe78432b42f4e5c6427979da5
InesZambranoGonzalez <zambranogonzalezines@gmail.com>
2025-02-11 19:21:27 +0100

    update profit_per_warehouses_updated
    updated values since we drop products

--- proyecto Mintclassics.sql
+++ proyecto Mintclassics.sql
@@ -6,7 +6,8 @@ SELECT
     warehouses.warehousePctCap,
     COUNT(products.productCode) AS total_products, 
     SUM(products.quantityInStock) AS total_stock,
-    SUM(products.MSRP - products.buyPrice) AS total_profit
+    SUM(products.MSRP - products.buyPrice) AS total_profit,
+    ( SUM(products.quantityInStock)*100/warehouses.warehousePctCap)AS max_cap
 FROM warehouses
 INNER JOIN products ON warehouses.warehouseCode = products.warehouseCode
 GROUP BY warehouses.warehouseCode;
@@ -22,15 +23,30 @@ INNER JOIN products ON orderdetails.productCode = products.productCode
 GROUP BY orderdetails.productCode 
 ORDER BY total_times_bought DESC; 
 
+/* */
 CREATE OR REPLACE VIEW `less_than_average_performance_by_products` AS
 SELECT popular_products_bought.total_times_bought , products.*
 FROM popular_products_bought 
 right JOIN products ON popular_products_bought.productCode = products.productCode
 WHERE total_times_bought < (SELECT (AVG(total_times_bought)-1) FROM popular_products_bought);
-	
+
+CREATE OR REPLACE VIEW `profit_per_warehouses_updated` AS
+SELECT 
+    profit_per_warehouses.warehouseCode,
+	((profit_per_warehouses.total_stock- SUM(less_than_average_performance_by_products.quantityInStock))/profit_per_warehouses.max_cap)*100 AS newWarehousePctCap,
+    profit_per_warehouses.max_cap,
+    profit_per_warehouses.total_products - COUNT(less_than_average_performance_by_products.productCode)AS newTotalProducts, 
+    profit_per_warehouses.total_stock- SUM(less_than_average_performance_by_products.quantityInStock) AS newTotalStock,
+    profit_per_warehouses.total_profit - SUM(less_than_average_performance_by_products.MSRP - less_than_average_performance_by_products.buyPrice) AS newTotalProfit
+    
+FROM profit_per_warehouses
+INNER JOIN less_than_average_performance_by_products ON profit_per_warehouses.warehouseCode = less_than_average_performance_by_products.warehouseCode
+GROUP BY profit_per_warehouses.warehouseCode;
+
 SELECT * from less_than_average_performance_by_products;
 SELECT * from popular_products_bought;
 SELECT * from profit_per_warehouses;
-
+SELECT * from profit_per_warehouses_updated;
+SELECT * from warehouses;
 
 
